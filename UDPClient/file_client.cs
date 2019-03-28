13968edcdeb4aeaf30b7deaf34e299780d6bb3ae
Jonas <jonas.hingebjerg@gmail.com>
2019-03-28 11:16:34 +0100

    Client

---
+++ UDPClient/file_client.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPClient
+{
+    class FileClient
+    {
+
+
+        const int BUFSIZE = 1024;
+        const int PORT = 9000;
+        const string ServerIP = "10.0.0.1";
+        private FileClient(string[] args) {
+            
+                   
+            
+            while(true){
+
+                Console.WriteLine("Enter l or L to get the server loadaverage");
+                Console.WriteLine("Enter u og U to get the server uptime");
+
+
+                byte[] CommandBytes = ReadLineAndConvert();
+
+
+                UdpClient Client = new UdpClient();
+                IPEndPoint ClientEnd = new IPEndPoint(IPAddress.Parse(ServerIP), PORT);
+
+                Client.Connect(ClientEnd);
+                Client.Send(CommandBytes, CommandBytes.Length);
+                Client.Close();
+
+                UdpClient Server = new UdpClient(PORT);
+                IPEndPoint ServerEnd = new IPEndPoint(IPAddress.Any, PORT);
+
+                string RecievedString = RecieveByteAndConvert(Server, ServerEnd);
+                
+                Console.WriteLine("Data recieved:");
+                Console.WriteLine(RecievedString);
+            }
+
+        }
+        
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting up ...");
+            new FileClient(args);
+        }
+        
+        private byte[] ReadLineAndConvert(){
+            string CommandString = Console.ReadLine();
+            byte[] CommandBytes = Encoding.ASCII.GetBytes(CommandString);
+            
+            return CommandBytes;
+        }
+        
+        
+        private string RecieveByteAndConvert(UdpClient server, IPEndPoint serverEnd){
+            byte[] RecievedBytes = server.Receive(ref serverEnd);
+            string RecievedString = Encoding.ASCII.GetString(RecievedBytes);
+
+            return RecievedString;
+        }
+    }
+}
